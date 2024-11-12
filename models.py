210018bed7a5cde38dac96d975f3427e1574a3af
phoenixt3ch <kocil123@gmail.com>
2024-11-13 00:19:24 +0300

    add remaining models

--- models.py
+++ models.py
@@ -15,3 +15,28 @@ class Client(db.Model):
     client_name = db.Column(db.String(50), nullable=False)
     client_phone = db.Column(db.String(50), nullable=False)
     client_email = db.Column(db.String(50))
+
+class Realty(db.Model):
+    __tablename__ = 'Realty'
+    realty_id = db.Column(db.Integer, primary_key=True)
+    realty_address = db.Column(db.String(50), nullable=False)
+    realty_square = db.Column(db.Integer, nullable=False)
+
+class Rent(db.Model):
+    __tablename__ = 'Rent'
+    rent_id = db.Column(db.Integer, primary_key=True)
+    agent_id = db.Column(db.Integer, db.ForeignKey('Agents.agent_id'), nullable=False)
+    realty_id = db.Column(db.Integer, db.ForeignKey('Realty.realty_id'), nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('Clients.client_id'), nullable=False)
+    rent_beginning_date = db.Column(db.Date, nullable=False)
+    rent_ending_date = db.Column(db.Date)
+    rent_price = db.Column(db.Integer, nullable=False)
+
+class Sale(db.Model):
+    __tablename__ = 'Sales'
+    sale_id = db.Column(db.Integer, primary_key=True)
+    agent_id = db.Column(db.Integer, db.ForeignKey('Agents.agent_id'), nullable=False)
+    realty_id = db.Column(db.Integer, db.ForeignKey('Realty.realty_id'), nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('Clients.client_id'), nullable=False)
+    sale_date = db.Column(db.Date, nullable=False)
+    sale_price = db.Column(db.Integer, nullable=False)
